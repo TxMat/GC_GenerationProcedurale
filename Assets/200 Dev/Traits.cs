a7bb2ee3bd3cf5ab5def09f5921809f37768d3b8
Thomas Dhaussy <94963203+DHS5@users.noreply.github.com>
2024-01-31 14:18:23 +0100

    Update Traits.cs

--- Assets/200 Dev/Traits.cs
+++ Assets/200 Dev/Traits.cs
@@ -8,6 +8,11 @@ public abstract class Traits : ScriptableObject
     [SerializeField] private int id;
     public int ID => id;
 
+    [SerializeField] private TraitTags tags;
+
+    [Header("Exclude")]
+    [SerializeField] private TraitTags excludeTags;
+
     public virtual void Init(int _id)
     {
         id = _id;
