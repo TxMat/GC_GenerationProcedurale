fed174f5c249bd690c1c4a4ef6de276a4d93f252
Tx Mat <46858765+TxMat@users.noreply.github.com>
2024-01-31 14:23:10 +0100

    orga

--- Assets/200 Dev/Traits.cs
+++ Assets/200 Dev/Traits.cs
@@ -26,58 +26,6 @@ public enum Category
     PERSONNALITY = 2,
 }
 
-public enum Jobs
-{
-    MEDIC = 0,
-    PAINTER = 1,
-    ENGINEER = 2,
-    STUDENT = 3,
-    FARMER = 4,
-    LAWYER = 5,
-    MUSICIAN = 6,
-    SCIENTIST = 7,
-    SOLDIER = 8,
-    ARTISAN = 9,
-    VETERAN = 10,
-}
-
-public enum Personalities
-{
-    ADVENTUROUS = 0,
-    SHY = 1,
-    CHARISMATIC = 2,
-    INTROVERT = 3,
-    OPTIMISTIC = 4,
-    PESSIMISTIC = 5,
-    ALTRUISTIC = 6,
-    EGOTIST = 7,
-    ANGRY = 8,
-    CALM = 9,
-    REALIST = 10,
-    DREAMER = 11,
-}
-
-public enum Past
-{
-    ORPHAN = 0,
-    EDUCATED = 1,
-    ANALPHABET = 2,
-    SINGLE = 3,
-    MARRIED = 4,
-    DIVORCED = 5,
-    WIDOW = 6,
-
-}
-
-public enum LifeStyle
-{
-    VOYAGER = 0,
-    CITY_HABITANT = 1,
-    COUNTRY_HABITANT = 2,
-    DISABLED = 5,
-    EXCENTRIC = 6,
-}
-
 [Flags]
 public enum TraitTags
 {
